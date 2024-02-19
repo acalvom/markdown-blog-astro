ac26b1fc161941e82bbbe3449e0dc01821159ba9
acalvom <andreac.3108@gmail.com>
2024-02-19 12:58:15 +0100

    feat: add Post types & utils for dates & refactor utils

---
+++ src/interfaces/Post.ts
@@ -0,0 +1,12 @@
+export type TPostMeta = {
+  title: string
+  description: string
+  pubDate: string
+  author: string
+  image: {
+    src: string
+    alt: string
+  }
+  file?: string
+  url?: string
+}
