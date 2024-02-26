f0ce1e4bd772facc31fddfd57bea8293b6aef113
acalvom <andreac.3108@gmail.com>
2024-02-26 12:38:28 +0100

    feat: add endpoint and page to get workout by id & mock response

--- src/services/workout.ts
+++ src/services/workout.ts
@@ -1,13 +1,34 @@
 import type { Workout } from '@interfaces/Workout'
 
+// TODO include library for fetch
 export const getAllWorkouts = async (): Promise<Array<Workout>> => {
-  const response = await fetch('https://exercisedb.p.rapidapi.com/exercises?limit=10', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-RapidAPI-Key': import.meta.env.PUBLIC_API_KEY,
-      'X-RapidAPI-Host': import.meta.env.PUBLIC_API_HOST
-    }
-  })
-  return await response.json()
+  try {
+    const response = await fetch('https://exercisedb.p.rapidapi.com/exercises?limit=10', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-RapidAPI-Key': import.meta.env.PUBLIC_API_KEY,
+        'X-RapidAPI-Host': import.meta.env.PUBLIC_API_HOST
+      }
+    })
+    return await response.json()
+  } catch (error: any) {
+    throw new Error('Failed to fetch workouts', error)
+  }
+}
+
+export const getWorkoutById = async ({ id }: { id: string }): Promise<Workout> => {
+  try {
+    const response = await fetch(`https://exercisedb.p.rapidapi.com/exercises/exercise/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-RapidAPI-Key': import.meta.env.PUBLIC_API_KEY,
+        'X-RapidAPI-Host': import.meta.env.PUBLIC_API_HOST
+      }
+    })
+    return await response.json()
+  } catch (error: any) {
+    throw new Error('Failed to fetch workout' + id, error)
+  }
 }
