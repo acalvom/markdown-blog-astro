a928acd174f266df9e29b3fc77aadba62b01d794
acalvom <andreac.3108@gmail.com>
2024-03-04 17:51:39 +0100

    feat: create button component

--- src/components/Shared/Button.tsx
+++ src/components/Shared/Button.tsx
@@ -1,20 +1,25 @@
 interface ButtonProps {
-  path: string
+  onClick: () => void
   icon?: boolean
-  children: React.ReactNode | React.ReactNode[]
+  children?: React.ReactNode | React.ReactNode[]
+  customStyle?: string
 }
 
-export const Button = ({ path, icon = false, children }: ButtonProps) => {
+export const Button = ({ onClick, icon = false, children, customStyle = '' }: ButtonProps) => {
   const iconStyle = !icon
-  ? 'rounded-md border border-red-400 transition duration-200 ease-out hover:-translate-y-1 hover:scale-110 hover:border-none hover:bg-red-300 hover:text-red-900 hover:shadow-md hover:shadow-current'
-  : ''
-    
+    ? 'rounded-md border border-red-400 transition duration-200 ease-out hover:-translate-y-1 hover:scale-110 hover:border-none hover:bg-red-300 hover:text-red-900 hover:shadow-md hover:shadow-current'
+    : ''
+
   return (
-    <a
-      className={`mx-2 mb-2 mt-1 flex flex-row items-center gap-x-2 px-4 py-2 text-center text-sm ${iconStyle}`}
-      href={path}
+    <button
+      className={
+        customStyle === ''
+          ? `mx-2 mb-2 mt-1 flex flex-row items-center gap-x-2 px-4 py-2 text-center text-sm ${iconStyle}`
+          : customStyle
+      }
+      onClick={onClick}
     >
       {children}
-    </a>
+    </button>
   )
 }
