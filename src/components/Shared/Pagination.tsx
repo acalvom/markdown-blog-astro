40d964dbe8e59ea64c37f1a5f61c06f4c4b61b26
acalvom <andreac.3108@gmail.com>
2024-03-05 14:54:37 +0100

    chore: refactor Pagination & add disable logic

--- src/components/Shared/Pagination.tsx
+++ src/components/Shared/Pagination.tsx
@@ -7,27 +7,19 @@ interface PaginationProps {
 }
 
 export const Pagination = ({ page, setPage }: PaginationProps) => {
-  const handleBack = () => {
-    if (page === 0) return
-    setPage(page - 1)
-  }
-
-  const handleNext = () => {
-    setPage(page + 1)
-  }
-
   return (
-    <div className="my-4 flex justify-center">
+    <div className="my-4 flex justify-between md:justify-around">
       <button
-        className="me-3  flex h-8 items-center justify-center gap-2 rounded-lg border border-cyan-300 bg-cyan-300 px-3 text-sm font-medium text-cyan-900 hover:bg-cyan-200 hover:text-cyan-700 "
-        onClick={handleBack}
+        className="ms-4 flex h-8 items-center justify-center gap-2 rounded-lg border border-cyan-300 bg-cyan-300 px-3 text-sm font-medium text-cyan-900 hover:bg-cyan-100 hover:text-cyan-700 disabled:border-none disabled:bg-gray-300 disabled:text-gray-700"
+        onClick={() => setPage(page - 1)}
+        disabled={page === 0}
       >
         <Icon paths={arrowBackIconPaths} />
         Previous
       </button>
       <button
-        className="me-3  flex h-8 items-center justify-center gap-2 rounded-lg border border-cyan-300 bg-cyan-300 px-3 text-sm font-medium text-cyan-900 hover:bg-cyan-200 hover:text-cyan-700 "
-        onClick={handleNext}
+        className="me-4 flex h-8 items-center justify-center gap-2 rounded-lg border border-cyan-300 bg-cyan-300 px-3 text-sm font-medium text-cyan-900 hover:bg-cyan-100 hover:text-cyan-700"
+        onClick={() => setPage(page + 1)}
       >
         Next
         <Icon paths={arrowNextIconPaths} />
